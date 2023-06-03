d44c4cfb5f2d5377b3b08671878f7b39ae127ec1
racalixta <ra.calixta@gmail.com>
2023-06-02 22:39:47 -0300

    conteudo da aula 05

---
+++ aula_04_flutter_app/lib/models/moeda.dart
@@ -0,0 +1,14 @@
+class Moeda {
+  String icone;
+  String nome;
+  String sigla;
+  double preco;
+
+  Moeda({
+    required this.icone,
+    required this.nome,
+    required this.sigla,
+    required this.preco,
+  });
+  
+}
--- aula_04_flutter_app/lib/pages/moedas_pages.dart
+++ aula_04_flutter_app/lib/pages/moedas_pages.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:aula_04_flutter_app/repositories/moeda_repository.dart';
 
 class MoedasPage extends StatelessWidget {
   const MoedasPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final tabela = MoedaRepository.tabela;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cripto Moedas'),
       ),
-      body: const Center(
-        child: Text('Olá'),
+      body: ListView.separated(
+        itemBuilder: (BuildContext context, int moeda) {
+          return ListTile(
+            leading: Image.asset(tabela[moeda].icone),
+            title: Text(tabela[moeda].nome),
+            trailing: Text(tabela[moeda].preco.toString()),
+          );
+        },
+        padding: const EdgeInsets.all(16),
+        separatorBuilder: (_, ___) => const Divider(),
+        itemCount: tabela.length,
       ),
     );
   }
